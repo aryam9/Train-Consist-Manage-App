bdec85c38ff5d87b246a80628cda353402141333
aryam9 <aryamurugan007@gmail.com>
2026-03-25 09:28:20 +0530

    UC3: Track Unique Bogie IDs

--- src/TCM.java
+++ src/TCM.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 public class TCM {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
         List<String> trainConsist = new ArrayList<>();
         System.out.println("Train initialized successfully.");
         System.out.println("Initial number of bogies: " + trainConsist.size());
+
         List<String> passengerBogies = new ArrayList<>();
         passengerBogies.add("Sleeper");
         passengerBogies.add("AC Chair");
@@ -22,6 +25,20 @@ public class TCM {
         }
         System.out.println("\nFinal passenger bogie list:");
         System.out.println(passengerBogies);
+
+        Set<String> bogieIds = new HashSet<>();
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101"); // duplicate
+        bogieIds.add("BG102"); // duplicate
+
+        System.out.println("\nBogie IDs added :");
+        System.out.println("BG101, BG102, BG103, BG101, BG102");
+
+        System.out.println("\nUnique Bogie IDs :");
+        System.out.println(bogieIds);
+
         System.out.println("System ready for further operations...");
     }
 }
