e1f1bd3cc1b1d049fa030247590923dbffc4634c
aryam9 <aryamurugan007@gmail.com>
2026-03-25 09:23:45 +0530

    UC2:Add Passenger Bogies to Train

--- src/TCM.java
+++ src/TCM.java
@@ -6,6 +6,22 @@ public class TCM {
         List<String> trainConsist = new ArrayList<>();
         System.out.println("Train initialized successfully.");
         System.out.println("Initial number of bogies: " + trainConsist.size());
+        List<String> passengerBogies = new ArrayList<>();
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+        System.out.println("Passenger bogies added:");
+        System.out.println(passengerBogies);
+        passengerBogies.remove("AC Chair");
+        System.out.println("\nAfter removing 'AC Chair':");
+        System.out.println(passengerBogies);
+        if (passengerBogies.contains("Sleeper")) {
+            System.out.println("\n'Sleeper' bogie exists in the train.");
+        } else {
+            System.out.println("\n'Sleeper' bogie does not exist.");
+        }
+        System.out.println("\nFinal passenger bogie list:");
+        System.out.println(passengerBogies);
         System.out.println("System ready for further operations...");
     }
 }
